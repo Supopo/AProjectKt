ec68487daf25767f9791d0b55fff9622c935c2e7
Suuu <516868419@qq.com>
2023-09-05 11:00:04 +0800

    网络请求例子

--- app/src/main/java/com/zoo/xxx/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/zoo/xxx/viewmodel/MainViewModel.kt
@@ -1,7 +1,17 @@
 package com.zoo.xxx.viewmodel;
 
+import com.zoo.mvvmkt.ext.request
 import com.zoo.mvvmkt.viewmodel.BaseViewModel
+import com.zoo.xxx.network.apiService
 
 open class MainViewModel : BaseViewModel() {
+
+    fun getBanner() {
+        request({ apiService.getBanner() }, {
+
+        }, {
+
+        }, true, "正在获取...")
+    }
 
 }
