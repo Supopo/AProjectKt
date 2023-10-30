9d1cb9f4c8e16cec1f8bfde10fad907cc7986190
Suuu <516868419@qq.com>
2023-10-30 21:24:08 +0800

    退出二次点击确认

--- app/src/main/java/com/zoo/xxx/activity/MainActivity.kt
+++ app/src/main/java/com/zoo/xxx/activity/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.zoo.xxx.activity
 
 import android.os.Bundle
-import androidx.navigation.Navigation
-import com.zoo.xxx.R
+import android.widget.Toast
 import com.zoo.xxx.base.BaseActivity
 import com.zoo.xxx.databinding.ActivityMainBinding
 import com.zoo.xxx.viewmodel.MainViewModel
@@ -18,4 +17,16 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
     }
 
+
+    private var firstClickTime: Long = 0
+    override fun onBackPressed() {
+        val currentTime = System.currentTimeMillis()
+        val duration = currentTime - firstClickTime
+        if (duration in 0..2000) {
+            finish()
+        } else {
+            firstClickTime = currentTime
+            Toast.makeText(this, "再按一次退出应用", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
