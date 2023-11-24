47c68a5a6e5118bee4e693807a8e929d3fd435c2
Suuu <516868419@qq.com>
2023-11-24 19:45:21 +0800

    添加图片选择

--- app/src/main/java/com/zoo/xxx/fragment/MeFragment.kt
+++ app/src/main/java/com/zoo/xxx/fragment/MeFragment.kt
@@ -1,10 +1,28 @@
 package com.zoo.xxx.fragment
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.luck.picture.lib.basic.PictureSelector
+import com.luck.picture.lib.config.SelectMimeType
+import com.luck.picture.lib.engine.CompressFileEngine
+import com.luck.picture.lib.engine.UriToFileTransformEngine
+import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.interfaces.OnKeyValueResultCallbackListener
+import com.luck.picture.lib.interfaces.OnPermissionsInterceptListener
+import com.luck.picture.lib.interfaces.OnRequestPermissionListener
+import com.luck.picture.lib.interfaces.OnResultCallbackListener
+import com.luck.picture.lib.thread.PictureThreadUtils.runOnUiThread
+import com.luck.picture.lib.utils.SandboxTransformUtils
 import com.zoo.mvvmkt.ext.nav
 import com.zoo.mvvmkt.ext.navigateAction
 import com.zoo.mvvmkt.util.ActivityMessenger
+import com.zoo.mvvmkt.util.FileUtil
+import com.zoo.mvvmkt.util.GlideEngine
 import com.zoo.xxx.R
 import com.zoo.xxx.activity.LoginActivity
 import com.zoo.xxx.appViewModel
@@ -14,6 +32,9 @@ import com.zoo.xxx.viewmodel.MeViewModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import top.zibin.luban.Luban
+import top.zibin.luban.OnNewCompressListener
+import java.io.File
 
 /**
  * Created by SuperPer'GPT on 2023/08/24.
@@ -43,6 +64,10 @@ class MeFragment : BaseFragment<MeViewModel, FragmentMeBinding>() {
         fun toSetting() {
             nav().navigateAction(R.id.to_fragment_setting)
         }
+
+        fun toSelectPhoto(){
+            checkPic()
+        }
     }
 
 
@@ -59,5 +84,102 @@ class MeFragment : BaseFragment<MeViewModel, FragmentMeBinding>() {
     override fun onHiddenChanged(hidden: Boolean) {
         super.onHiddenChanged(hidden)
         Log.d(TAG, "hidden: $hidden")
+    }
+
+    //选择图片
+    private fun checkPic() {
+
+        // 进入相册
+
+        PictureSelector.create(this)
+            .openGallery(SelectMimeType.ofImage())
+            .setImageEngine(GlideEngine.createGlideEngine())
+            .setMaxSelectNum(1)
+            .setPermissionsInterceptListener(object :OnPermissionsInterceptListener{
+                override fun requestPermission(
+                    fragment: Fragment?,
+                    permissionArray: Array<out String>?,
+                    call: OnRequestPermissionListener?
+                ) {
+                    // 1、发起权限申请
+                    // 2、call.onCall();返回申请状态
+                }
+
+                override fun hasPermissions(
+                    fragment: Fragment?,
+                    permissionArray: Array<out String>?
+                ): Boolean {
+                    // 验证权限是否申请成功
+                    return false
+                }
+
+            })
+            .setSandboxFileEngine(object : UriToFileTransformEngine {
+                override fun onUriToFileAsyncTransform(
+                    context: Context?,
+                    srcPath: String?,
+                    mineType: String?,
+                    call: OnKeyValueResultCallbackListener?
+                ) {
+                    if (call != null) {
+                        val sandboxPath =
+                            SandboxTransformUtils.copyPathToSandbox(context, srcPath, mineType)
+                        call.onCallback(srcPath, sandboxPath)
+                    }
+                }
+            })
+            .setCompressEngine(object : CompressFileEngine {
+                override fun onStartCompress(
+                    context: Context?,
+                    source: java.util.ArrayList<Uri>?,
+                    call: OnKeyValueResultCallbackListener?
+                ) {
+                    //压缩
+                    Luban.with(context).load(source).ignoreBy(100)
+                        .setCompressListener(object : OnNewCompressListener {
+                            override fun onStart() {
+                            }
+
+                            override fun onSuccess(source: String?, compressFile: File?) {
+                                call?.onCallback(source, compressFile!!.getAbsolutePath())
+                            }
+
+                            override fun onError(source: String?, e: Throwable?) {
+                                call?.onCallback(source, null)
+                            }
+                        }).launch()
+                }
+            })
+            .forResult(object : OnResultCallbackListener<LocalMedia?> {
+                override fun onResult(result: ArrayList<LocalMedia?>) {
+
+                    for (localMedia in result) {
+
+                        //获取源路径 但在Android Q版本上返回的是content:// Uri类型
+                        val path = FileUtil.getFilePath(activity, Uri.parse(localMedia!!.path))
+                        var compressedPath = path
+                        //如果有压缩去压缩后路径
+                        if (localMedia.isCompressed) {
+                            compressedPath = localMedia.compressPath
+                        }
+
+                        runOnUiThread {
+                            Glide.with(this@MeFragment)
+                                .load(
+                                    compressedPath
+                                )
+                                .centerCrop()
+                                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                                .into(
+                                    mDatabind.ivPhoto
+                                )
+                        }
+                    }
+                }
+
+                override fun onCancel() {
+
+                }
+            })
     }
 }
